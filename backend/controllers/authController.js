b82ef4970041939007e2bd17d64869bbd8dcc6c8
awais512 <raza.awais25@gmail.com>
2020-12-19 17:29:27 +0500

    Account Activation

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,6 +3,9 @@ const jwt = require('jsonwebtoken');
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
+//@desc     Register User
+//@route    POST /api/v1/auth/signup
+//@access   Public
 exports.signup = async (req, res) => {
   const { name, email, password } = req.body;
   try {
@@ -36,5 +39,26 @@ exports.signup = async (req, res) => {
   } catch (err) {
     console.log(err.response.body);
     res.status(500).json({ err: 'Server Error' });
+  }
+};
+
+//@desc     Activate Account
+//@route    POST /api/v1/auth/activate
+//@access   Public
+exports.accountActivation = async (req, res) => {
+  const { token } = req.body;
+
+  try {
+    if (token) {
+      jwt.verify(token, process.env.JWT_ACCOUNT_ACTIVATION);
+      const { name, email, password } = jwt.decode(token);
+      const user = await new User({ name, email, password }).save();
+      return res.json({
+        message: 'Signedup Successfully! Please Sign In',
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: 'Something Went Wrong', error: err });
   }
 };
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { signup } = require('../controllers/authController');
+const { signup, accountActivation } = require('../controllers/authController');
 const { runValidation } = require('../validators');
 const { signupValidator } = require('../validators/auth');
 
 router.post('/signup', signupValidator, runValidation, signup);
+router.post('/account-activation', accountActivation);
 
 module.exports = router;

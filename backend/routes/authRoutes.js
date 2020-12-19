b88c80d32abd0347a43f6c30c66c10567f0984dd
awais512 <raza.awais25@gmail.com>
2020-12-19 14:45:26 +0500

    Validate Request

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { signup } = require('../controllers/authController');
+const { runValidation } = require('../validators');
+const { signupValidator } = require('../validators/auth');
 
-router.get('/signup', signup);
+router.post('/signup', signupValidator, runValidation, signup);
 
 module.exports = router;

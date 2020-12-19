f5ff5f6fa84f57d52a4f785368882e1208ec5743
awais512 <raza.awais25@gmail.com>
2020-12-19 12:46:57 +0500

    Apply packages middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -9,9 +10,14 @@ const authRoutes = require('./routes/authRoutes');
 
 //Express Body Parser
 app.use(express.json());
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(cors());
 
 //Mounting Routes
 app.use('/api/v1/auth', authRoutes);
 
-app.listen(5000, console.log('Server is Running on port 5000'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server is Running on port ${PORT}`));

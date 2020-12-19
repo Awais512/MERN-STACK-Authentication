81e4fdbe5f0092ced298f1dae59239bfd22b7d0d
awais512 <raza.awais25@gmail.com>
2020-12-19 15:07:03 +0500

    User Signed up

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,19 @@
+const User = require('../models/userModel');
+
 exports.signup = async (req, res) => {
+  const { name, email, password } = req.body;
   try {
-    res.json({ message: 'User Signed Up' });
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ error: 'Email already exist' });
+    }
+    user = await new User({ name, email, password }).save();
+
+    res
+      .status(201)
+      .json({ msg: 'User Signed Up successfully! Please Sign in', data: user });
   } catch (err) {
     console.log(err);
+    res.status(500).json({ err: 'Server Error' });
   }
 };

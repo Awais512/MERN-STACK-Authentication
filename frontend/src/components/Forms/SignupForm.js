369eb8f3829a62669194cb35f983dd55ec24af2c
awais512 <raza.awais25@gmail.com>
2020-12-21 10:17:55 +0500

    Frontend Validation

--- frontend/src/components/Forms/SignupForm.js
+++ frontend/src/components/Forms/SignupForm.js
@@ -10,36 +10,39 @@ const SignupForm = ({ values, setValues }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    try {
-      if (!name) {
-        return toast.error('Name is Required');
-      } else if (!password) {
-        return toast.error('Password is Required');
-      } else if (password.length < 6) {
-        return toast.error('Password should be atleast 6 characters');
-      } else if (!email) {
-        return toast.error('Email is Required');
+    if (!name) {
+      return toast.error('Name is Required');
+    } else if (name.length < 4) {
+      return toast.error('Name should be atleast 4 Characters');
+    } else if (!password) {
+      return toast.error('Password is Required');
+    } else if (password.length < 6) {
+      return toast.error('Password should be atleast 6 characters');
+    } else if (!email) {
+      return toast.error('Email is Required');
+    } else {
+      try {
+        const { data } = await registerUser({ name, email, password });
+        console.log(data);
+        setValues({
+          ...values,
+          name: '',
+          email: '',
+          password: '',
+          btnText: 'Submit',
+        });
+        toast.success(data.message);
+      } catch (error) {
+        console.log(error);
+        setValues({
+          ...values,
+          name: '',
+          email: '',
+          password: '',
+          btnText: 'Submit',
+        });
+        toast.error('Email Already Exist');
       }
-      const { data } = await registerUser({ name, email, password });
-      console.log(data);
-      setValues({
-        ...values,
-        name: '',
-        email: '',
-        password: '',
-        btnText: 'Submit',
-      });
-      toast.success(data.message);
-    } catch (error) {
-      console.log(error);
-      setValues({
-        ...values,
-        name: '',
-        email: '',
-        password: '',
-        btnText: 'Submit',
-      });
-      toast.error('Email Already Exist');
     }
   };
   return (
@@ -73,7 +76,13 @@ const SignupForm = ({ values, setValues }) => {
       </div>
       <div className='pt-2'>
         <button
-          disabled={!name || !email || !password}
+          disabled={
+            !name ||
+            name.length < 4 ||
+            !email ||
+            !password ||
+            password.length < 6
+          }
           className='btn btn-primary'
           onClick={handleSubmit}
         >
